1d1b0b5f2058ca752f243ac953b8f74b5d6472c4
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-08 23:13:27 +0530

    Update second.c

--- second.c
+++ second.c
@@ -2,10 +2,10 @@
 void main()
 {
 int n;
-printf("enter the no:")
-scanf("%d",&n)
+printf("enter the no:");
+scanf("%d",&n);
 if(n%2==0)
-printf("no is even")
+printf("no is even");
 else
 printf("no is odd);
 }
