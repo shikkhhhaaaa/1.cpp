f09e7165dda14c935c9ce7dd26427ea361155f79
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-08 22:20:22 +0530

    first.cpp

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  printf("enter the no");
+  scanf("%d",&n);
+  if(n<0)
+  printf("no is negative");
+  else
+  if(n>0)
+  printf("no is positive");
+  else
+  print(no: is zero);
+  }
+  
