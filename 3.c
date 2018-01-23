db1cb39efd4015ef702322ca87db0a481fbf6e5a
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-23 22:14:09 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -4,7 +4,7 @@ void main()
 char vowel;
 printf("enter the character");
 scanf("%d",&vowel);
-if(vowel=='a'||vowel=='A'||vowel=='e'||vowel=='E'||vowel=='i'||vowel=='I'||vowel=='O'||vowel=='o'||vowel=='u'||vowel=='U');
+if(vowel=='a'||vowel=='A'||vowel=='e'||vowel=='E'||vowel=='i'||vowel=='I'||vowel=='O'||vowel=='o'||vowel=='u'||vowel=='U')
 printf("character is vowel");
 else
 printf("it is consonant");
