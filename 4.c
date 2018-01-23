0f575f0c437264e700cb408ffddd8b59681a475e
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-23 22:30:33 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	char ch;
-	printf("enter the character");
-	scanf("%c",&ch);
-	if(ch>0&&ch<0&&ch==0)
-	printf("it is not alphabet");
-	else
-	printf("it is alphabet");
-	return(0);
+
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%c is an alphabet.",c);
+    else
+        printf("%c is not an alphabet.",c);
+
+    return 0;
 }
+
