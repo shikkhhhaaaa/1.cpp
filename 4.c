de16650cc8d7a53e97b8570bf98e575d21a46b30
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-23 22:26:01 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("enter the character");
+	scanf("%c",&ch);
+	if(ch>0&&ch<0&&ch==0)
+	printf("it is not alphabet");
+	else
+	printf("it is alphabet");
+	return(0);
+}
