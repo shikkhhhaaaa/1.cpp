32705bf9641a55366f47ac0461aa19a3c4380977
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-23 22:09:56 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char vowel;
+printf("enter the character");
+scanf("%d",&vowel);
+if(vowel=='a'||vowel=='A'||vowel=='e'||vowel=='E'||vowel=='i'||vowel=='I'||vowel=='O'||vowel=='o'||vowel=='u'||vowel=='U');
+printf("character is vowel");
+else
+printf("it is consonant");
+}
