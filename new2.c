027172d3b67b79e728774cb38ba7a0690d2ca769
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-25 00:35:41 +0530

    Create new2.c

---
+++ new2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n,k,s;
+
+    printf("Enter the no: ");
+    scanf("%d", &n);
+    n=k;
+	while(n!=0)
+	{
+		s=n%10;
+		n=n/10;
+		
+	}
+	if(k==s)
+	printf("no is palindrome");
+	else
+	printf("no it is not palindrome");
+}
