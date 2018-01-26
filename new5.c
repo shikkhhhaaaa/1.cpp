94e471345b3a48b466b9103f3985f25a05457583
shikkhhhaaaa <35231560+shikkhhhaaaa@users.noreply.github.com>
2018-01-27 00:32:15 +0530

    Create new5.c

---
+++ new5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,d,flag=0;
+	printf("enter the interval of the number");
+	scanf("%d %d",&n,&d);
+	for(i=n;i<d;i++)
+	{
+		
+	if(i%2!=0||i==n)
+	{
+		flag=0;
+		
+	}
+	else
+	{
+		flag=1;
+		printf("%d\n",i);
+	}
+	
+}
+}
